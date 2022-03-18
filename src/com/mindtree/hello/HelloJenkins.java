1ec4e377ba37a82b30d4ece2019ad66d68dc360e
SomeshBanna <someshbanna845@gmail.com>
2022-03-18 09:10:35 +0530

    first Jenkins commit

---
+++ src/com/mindtree/hello/HelloJenkins.java
@@ -0,0 +1,10 @@
+package com.mindtree.hello;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello jenkins");
+	}
+
+}
